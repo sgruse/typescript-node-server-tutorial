a193f1bfcce80e0ca0cd53ed50dcc8096343db0c
sgruse <sgruse89@gmail.com>
2017-12-13 09:36:37 -0800

    Trimmed down the project structure

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
-// import * as http from 'http'
-
-// import App from './App'
-
-// const server = http.createServer(App);
-// const port = 3000
-
-// server.listen(port);
-// server.on('listening', onListening);
-
-// function onListening(): void {
-// 	console.log('Bubbles are served at: ', port)
-// 	let addr = server.address();
-// 	let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
-// }
-
 import * as http from 'http'
 
-// import App from './App'
-
-import Server from './newApp'
-
-const App = Server.createServerInstance()
+import App from './App'
 
 const server = http.createServer(App);
 const port = 3000
@@ -29,7 +9,8 @@ server.listen(port);
 server.on('listening', onListening);
 
 function onListening(): void {
-	console.log('Bubbles are served at: ', port)
+	console.log('Server Listening at: ', port)
 	let addr = server.address();
 	let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
 }
+
